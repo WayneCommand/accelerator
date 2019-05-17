3c962f8e3c19bdcb647838ab8ae2aca74030c4d4
shenlanAZ <shenlanluck@gmail.com>
2019-05-17 13:11:03 +0800

    add hikariCP datasource pool,using mariaDB

--- sql/t_user.sql
+++ sql/t_user.sql
@@ -1,17 +1,17 @@
 /*
  Navicat MySQL Data Transfer
 
- Source Server         : localhost_3306
- Source Server Type    : MySQL
- Source Server Version : 50548
+ Source Server         : localhost_mariadb
+ Source Server Type    : MariaDB
+ Source Server Version : 100315
  Source Host           : localhost:3306
  Source Schema         : mycloud
 
- Target Server Type    : MySQL
- Target Server Version : 50548
+ Target Server Type    : MariaDB
+ Target Server Version : 100315
  File Encoding         : 65001
 
- Date: 16/05/2019 21:18:44
+ Date: 17/05/2019 13:05:39
 */
 
 SET NAMES utf8mb4;
@@ -27,10 +27,9 @@ CREATE TABLE `t_user`  (
   `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '密码',
   `salt` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '盐值',
   `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '邮箱',
-  `last_login` timestamp NULL DEFAULT NULL COMMENT '最后登录时间',
-  `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
-  `update_time` datetime NULL DEFAULT NULL COMMENT '最后资料更新时间',
+  `create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime(0) NULL DEFAULT NULL COMMENT '最后资料更新时间',
   PRIMARY KEY (`u_id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Compact;
+) ENGINE = Aria AUTO_INCREMENT = 0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Page;
 
 SET FOREIGN_KEY_CHECKS = 1;

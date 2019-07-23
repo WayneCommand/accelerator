e26708d3503d92f5a64389d8ced166a49e822310
shenlanAZ <shenlanluck@gmail.com>
2019-07-23 10:03:56 +0800

    update LOCK thanks@bufflu

--- src/main/java/ltd/inmind/user_service/utils/KVPlusMap.java
+++ src/main/java/ltd/inmind/user_service/utils/KVPlusMap.java
@@ -9,8 +9,6 @@ public class KVPlusMap<K, V> extends AbstractMap<K, V> {
     private Map<K, V> kvHashMap = new HashMap<>();
     private Map<K, Long> expiryMap = new HashMap<>();
 
-    private static final byte[] LOCK = new byte[0];
-
     /**
      * default expiry time (ms).
      * if value = -1, this kv never expired.
@@ -18,59 +16,49 @@ public class KVPlusMap<K, V> extends AbstractMap<K, V> {
     private static final long DEFAULT_EXPIRED_TIME = -1L;
 
     @Override
-    public V put(K key, V value) {
-        synchronized (LOCK) {
-            V put = kvHashMap.put(key, value);
-            expiryMap.put(key, DEFAULT_EXPIRED_TIME);
-            return put;
-        }
+    public synchronized V put(K key, V value) {
+        V put = kvHashMap.put(key, value);
+        expiryMap.put(key, DEFAULT_EXPIRED_TIME);
+        return put;
     }
 
-    public V put(K key, V value,long expiryTime) {
-        synchronized (LOCK) {
-            if (expiryTime < 1)
-                return null;
+    public synchronized V put(K key, V value,long expiryTime) {
+        if (expiryTime < 1)
+            return null;
 
-            V put = kvHashMap.put(key, value);
-            expiryMap.put(key, System.currentTimeMillis() + expiryTime);
-            return put;
-        }
+        V put = kvHashMap.put(key, value);
+        expiryMap.put(key, System.currentTimeMillis() + expiryTime);
+        return put;
     }
 
 
 
     @Override
-    public V get(Object key) {
-        synchronized (LOCK){
-            if (isExpired(key))
-                return null;
-            return kvHashMap.get(key);
-        }
+    public synchronized V get(Object key) {
+        if (isExpired(key))
+            return null;
+        return kvHashMap.get(key);
     }
 
     @Override
-    public boolean remove(Object key, Object value) {
-        synchronized (LOCK){
-            if (isExpired(key, true)) {
-                return false;
-            }
-            kvHashMap.remove(key, value);
-            expiryMap.remove(key);
-            return true;
+    public synchronized boolean remove(Object key, Object value) {
+        if (isExpired(key, true)) {
+            return false;
         }
+        kvHashMap.remove(key, value);
+        expiryMap.remove(key);
+        return true;
     }
 
 
     @Override
-    public V remove(Object key) {
-        synchronized (LOCK){
-            if (isExpired(key, true)) {
-                return null;
-            }
-
-            expiryMap.remove(key);
-            return kvHashMap.remove(key);
+    public synchronized V remove(Object key) {
+        if (isExpired(key, true)) {
+            return null;
         }
+
+        expiryMap.remove(key);
+        return kvHashMap.remove(key);
     }
 
     @Override
@@ -85,31 +73,27 @@ public class KVPlusMap<K, V> extends AbstractMap<K, V> {
      *
      * @return true = 过期 ， false = 有效。
      */
-    public boolean isExpired(Object key) {
-        synchronized (LOCK){
-            if (!expiryMap.containsKey(key))
-                return true;
+    public synchronized boolean isExpired(Object key) {
+        if (!expiryMap.containsKey(key))
+            return true;
 
-            Long expiryTime  = expiryMap.get(key);
+        Long expiryTime  = expiryMap.get(key);
 
-            if (expiryTime == DEFAULT_EXPIRED_TIME)
-                return false;
+        if (expiryTime == DEFAULT_EXPIRED_TIME)
+            return false;
 
-            return System.currentTimeMillis() > expiryTime;
-        }
+        return System.currentTimeMillis() > expiryTime;
     }
 
-    public boolean isExpired(Object key, boolean remove) {
-        synchronized (LOCK) {
-            if (isExpired(key)){
-                if (remove){
-                    kvHashMap.remove(key);
-                    expiryMap.remove(key);
-                }
-                return true;
+    public synchronized boolean isExpired(Object key, boolean remove) {
+        if (isExpired(key)){
+            if (remove){
+                kvHashMap.remove(key);
+                expiryMap.remove(key);
             }
-            return false;
+            return true;
         }
+        return false;
     }
 
 
@@ -122,9 +106,9 @@ public class KVPlusMap<K, V> extends AbstractMap<K, V> {
         int modCount = kvHashMap.size();
 
         kvHashMap.forEach((k, v) -> {
-            if (!isExpired(k)) {
+            if (!isExpired(k))
                 action.accept(k, v);
-            }
+
         });
 
         if (modCount != kvHashMap.size())

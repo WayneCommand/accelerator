b7c6535851be5c96c36b69cbfbe945b0c07a4e41
shenlanAZ <shenlanluck@gmail.com>
2019-08-15 14:54:15 +0800

    解决同源策略

--- src/main/java/ltd/inmind/user_service/config/WebConfig.java
+++ src/main/java/ltd/inmind/user_service/config/WebConfig.java
@@ -2,6 +2,7 @@ package ltd.inmind.user_service.config;
 
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,10 +12,18 @@ public class WebConfig implements WebMvcConfigurer {
     /**
      * 解决未授权的非法访问
      * 比如接口是/test 那么用/test/可以绕过filter
-     * @param configurer
      */
     @Override
     public void configurePathMatch(PathMatchConfigurer configurer) {
         configurer.setUseTrailingSlashMatch(false);
+    }
+
+    /**
+     * 同源策略（跨域）
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080");
     }
 }

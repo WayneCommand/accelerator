5c9c9d68d2f2387616b2bfa0f9e3644717e00ff8
shenlanAZ <shenlanluck@gmail.com>
2020-09-21 10:57:59 +0800

    func update.

--- src/main/java/ltd/inmind/accelerator/utils/KVPlusMap.java
+++ src/main/java/ltd/inmind/accelerator/utils/KVPlusMap.java
@@ -6,36 +6,92 @@ import java.util.stream.Collectors;
 
 /**
  * @author shenlan#https://github.com/shenlanAZ
- * @version 0.2
+ * @version 0.5
  */
 public class KVPlusMap<K, V> extends AbstractMap<K, V> {
 
-    private Map<K, V> kvHashMap = new HashMap<>();
-    private Map<K, Long> expiryMap = new HashMap<>();
+    private Map<K, V> kvHashMap;
+    private Map<K, Long> expiryMap;
 
     /**
      * default expiry time (ms).
      * if value = -1, this kv never expired.
      */
     private static final long DEFAULT_EXPIRED_TIME = -1L;
+    /**
+     * The load factor used when none specified in constructor.
+     */
+    private static final float DEFAULT_LOAD_FACTOR = 0.75f;
+    /**
+     * The default initial capacity - MUST be a power of two.
+     */
+    static final int DEFAULT_INITIAL_CAPACITY = 1 << 4; // aka 16
+    /**
+     * The value affect gc
+     *
+     * @serial
+     */
+    final int threshold = 1 << 20; // aka 1,048,576
+    /**
+     * The value affect gc run
+     */
+    int thresholdCount;
+
+    final float loadFactor;
+
+    public KVPlusMap() {
+        this(DEFAULT_INITIAL_CAPACITY, DEFAULT_LOAD_FACTOR);
+    }
+
+    public KVPlusMap(int initialCapacity) {
+        this(initialCapacity, DEFAULT_LOAD_FACTOR);
+    }
+
+    public KVPlusMap(int initialCapacity, float _loadFactor) {
+        loadFactor = _loadFactor;
+        kvHashMap = new HashMap<>(initialCapacity,loadFactor);
+        expiryMap = new HashMap<>(initialCapacity,loadFactor);
+    }
 
     @Override
     public synchronized V put(K key, V value) {
-        V put = kvHashMap.put(key, value);
-        expiryMap.put(key, DEFAULT_EXPIRED_TIME);
-        return put;
+        return put(key, value, DEFAULT_EXPIRED_TIME);
     }
 
     public synchronized V put(K key, V value,long expiryTime) {
-        if (expiryTime < 1)
-            return null;
+        if (expiryTime < 0)
+            expiryTime = DEFAULT_EXPIRED_TIME;
 
         V put = kvHashMap.put(key, value);
-        expiryMap.put(key, System.currentTimeMillis() + expiryTime);
+
+        if (expiryTime == DEFAULT_EXPIRED_TIME)
+            expiryMap.put(key, expiryTime);
+        else
+            expiryMap.put(key, System.currentTimeMillis() + expiryTime);
+
+        if (++thresholdCount > threshold){
+            gc();
+            thresholdCount = 0;
+        }
+
         return put;
     }
 
+    public synchronized void gc() {
+        int initialCapacity = size();
+        Map<K, V> _kvHashMap;
+        Map<K, Long> _expiryMap;
 
+        _kvHashMap = new HashMap<>(initialCapacity, loadFactor);
+        _expiryMap = new HashMap<>(initialCapacity, loadFactor);
+        forEach((key, value) -> {
+            _kvHashMap.put(key, value);
+            _expiryMap.put(key, expiryMap.get(key));
+        });
+
+        kvHashMap = _kvHashMap;
+        expiryMap = _expiryMap;
+    }
 
     @Override
     public synchronized V get(Object key) {
@@ -46,17 +102,20 @@ public class KVPlusMap<K, V> extends AbstractMap<K, V> {
 
     @Override
     public synchronized boolean remove(Object key, Object value) {
-        if (isExpired(key, true))
+        if (isExpired(key))
             return false;
 
-        expiryMap.remove(key);
-        return kvHashMap.remove(key, value);
+        boolean remove = kvHashMap.remove(key, value);
+        if (remove){
+            expiryMap.remove(key);
+        }
+        return remove;
     }
 
 
     @Override
     public synchronized V remove(Object key) {
-        if (isExpired(key, true))
+        if (isExpired(key))
             return null;
 
         expiryMap.remove(key);
@@ -65,21 +124,11 @@ public class KVPlusMap<K, V> extends AbstractMap<K, V> {
 
     @Override
     public Set<Entry<K, V>> entrySet() {
-        return kvHashMap.entrySet().stream().filter(entry -> isExpired(entry.getKey()))
+        return kvHashMap.entrySet().stream().filter(entry -> !isExpired(entry.getKey()))
                 .collect(Collectors.toSet());
     }
 
-
-    /**
-     * 是否已经过期 默认不移除
-     *
-     * @return true = 过期 ， false = 有效。
-     */
     private synchronized boolean isExpired(Object key) {
-        return isExpired(key, false);
-    }
-
-    private synchronized boolean isExpired(Object key, boolean remove) {
         if (!expiryMap.containsKey(key))
             return true;
 
@@ -88,19 +137,22 @@ public class KVPlusMap<K, V> extends AbstractMap<K, V> {
         if (expiryTime == DEFAULT_EXPIRED_TIME)
             return false;
 
-        if (System.currentTimeMillis() > expiryTime){
-            if (remove){
-                kvHashMap.remove(key);
-                expiryMap.remove(key);
-            }
-            return true;
-        }
-        return false;
+        return System.currentTimeMillis() > expiryTime;
+    }
+
+    @Override
+    public void forEach(BiConsumer<? super K, ? super V> action) {
+        int modCount = size();
+
+        super.forEach(action);
+
+        if (modCount != size())
+            throw new ConcurrentModificationException();
     }
 
     @Override
     public boolean containsKey(Object key) {
-        return !isExpired(key, true);
+        return !isExpired(key);
     }
 
     @Override
@@ -109,20 +161,7 @@ public class KVPlusMap<K, V> extends AbstractMap<K, V> {
     }
 
     @Override
-    public void forEach(BiConsumer<? super K, ? super V> action) {
-        if (action == null)
-            throw new NullPointerException();
-
-        int modCount = kvHashMap.size();
-
-        kvHashMap.forEach((k, v) -> {
-            if (!isExpired(k))
-                action.accept(k, v);
-
-        });
-
-        if (modCount != kvHashMap.size())
-            throw new ConcurrentModificationException();
+    public void clear() {
+        throw new UnsupportedOperationException();
     }
-
 }
